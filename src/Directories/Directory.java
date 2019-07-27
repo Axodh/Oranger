e13e0d77c128538a27adc92dbad911d6660a4aea
Axodh <37446105+Axodh@users.noreply.github.com>
2019-07-27 15:24:37 +0200

    Base

---
+++ src/Directories/Directory.java
@@ -0,0 +1,17 @@
+package Directories;
+
+import java.io.File;
+
+
+public class Directory{
+
+    public File createDir(String path){
+        new File(path).mkdir();
+        return File;
+    }
+
+    public void deleteDir( String path ){
+
+    }
+
+}
---
+++ src/Directories/Main.java
@@ -0,0 +1,17 @@
+package Directories;
+
+public class Main {
+
+
+
+    public static void main(String args[]){
+
+        Directory dir = new Directory();
+
+
+        dir.createDir();
+
+    }
+
+
+}
